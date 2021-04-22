9e38ffe62ac18377683cc7f322e04f3a49edebf4
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-04-22 21:55:12 +0000

    improve readability

--- bin/check-cmds.js
+++ bin/check-cmds.js
@@ -7,19 +7,19 @@ const args = process.argv.slice(2);
 
 let isLegacy = false;
 if (args[0] === '--legacy') {
-	isLegacy = true;
-	args.splice(0, 1);
+    isLegacy = true;
+    args.splice(0, 1);
 }
 
 let isOutput = false;
 if (args[0] === '--output') {
-	isOutput = true;
-	args.splice(0, 1);
+    isOutput = true;
+    args.splice(0, 1);
 }
 
 const [hestiaRepo = 'https://github.com/hestiacp/hestiacp', hestiaBranch = 'main'] = args;
 const cmds = processCmds({ hestiaRepo, hestiaBranch, cache: false });
 
 if (isOutput) {
-	fs.writeFileSync(path.join(process.cwd(), 'hestia-cmds.json'), JSON.stringify(cmds, null, 2));
+    fs.writeFileSync(path.join(process.cwd(), 'hestia-cmds.json'), JSON.stringify(cmds, null, 2));
 }
